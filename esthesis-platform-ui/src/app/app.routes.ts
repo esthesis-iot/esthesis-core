e40ba07b1715bec146c0702b6082e6f31de3c0dd
Nassos Michas <nassosmichas@me.com>
2019-04-02 09:18:47 +0300

    Formatting.

--- esthesis-platform-ui/src/app/app.routes.ts
+++ esthesis-platform-ui/src/app/app.routes.ts
@@ -18,20 +18,23 @@ export const routing: ModuleWithProviders = RouterModule.forRoot([
   {path: 'forgot-password', component: ForgotPasswordComponent},
 
   // Features routes.
-  {path: 'dashboard', loadChildren: './dashboard/dashboard.module#DashboardModule', canActivate: [CanActivateGuard]},
-  {path: 'audit', loadChildren: './audit/audit.module#AuditModule', canActivate: [CanActivateGuard]},
-  {path: 'devices', loadChildren: './devices/devices.module#DevicesModule', canActivate: [CanActivateGuard]},
-  {path: 'profile', loadChildren: './profile/profile.module#ProfileModule', canActivate: [CanActivateGuard]},
-  {path: 'cas', loadChildren: './cas/cas.module#CasModule', canActivate: [CanActivateGuard]},
-  {path: 'certificates', loadChildren: './certificates/certificates.module#CertificatesModule', canActivate: [CanActivateGuard]},
-  {path: 'infra', loadChildren: './infrastructure/infrastructure.module#InfrastructureModule', canActivate: [CanActivateGuard]},
-  {path: 'provisioning', loadChildren: './provisioning/provisioning.module#ProvisioningModule', canActivate: [CanActivateGuard]},
-  {path: 'users', loadChildren: './users/users.module#UsersModule', canActivate: [CanActivateGuard]},
-  {path: 'settings', loadChildren: './settings/settings.module#SettingsModule', canActivate: [CanActivateGuard]},
-  {path: 'tags', loadChildren: './tags/tags.module#TagsModule', canActivate: [CanActivateGuard]},
-  {path: 'applications', loadChildren: './applications/applications.module#ApplicationsModule', canActivate: [CanActivateGuard]},
-  {path: 'datasinks', loadChildren: './datasinks/datasinks.module#DatasinksModule', canActivate: [CanActivateGuard]},
+  {path: 'dashboard',     loadChildren: './dashboard/dashboard.module#DashboardModule', canActivate: [CanActivateGuard]},
+  {path: 'audit',         loadChildren: './audit/audit.module#AuditModule', canActivate: [CanActivateGuard]},
+  {path: 'devices',       loadChildren: './devices/devices.module#DevicesModule', canActivate: [CanActivateGuard]},
+  {path: 'profile',       loadChildren: './profile/profile.module#ProfileModule', canActivate: [CanActivateGuard]},
+  {path: 'cas',           loadChildren: './cas/cas.module#CasModule', canActivate: [CanActivateGuard]},
+  {path: 'certificates',  loadChildren: './certificates/certificates.module#CertificatesModule', canActivate: [CanActivateGuard]},
+  {path: 'infra',         loadChildren: './infrastructure/infrastructure.module#InfrastructureModule', canActivate: [CanActivateGuard]},
+  {path: 'provisioning',  loadChildren: './provisioning/provisioning.module#ProvisioningModule', canActivate: [CanActivateGuard]},
+  {path: 'users',         loadChildren: './users/users.module#UsersModule', canActivate: [CanActivateGuard]},
+  {path: 'settings',      loadChildren: './settings/settings.module#SettingsModule', canActivate: [CanActivateGuard]},
+  {path: 'tags',          loadChildren: './tags/tags.module#TagsModule', canActivate: [CanActivateGuard]},
+  {path: 'applications',  loadChildren: './applications/applications.module#ApplicationsModule', canActivate: [CanActivateGuard]},
+  {path: 'datasinks',     loadChildren: './datasinks/datasinks.module#DatasinksModule', canActivate: [CanActivateGuard]},
 
   // Redirect all other urls to Dashboard.
   {path: '**', redirectTo: 'dashboard'}
-], {enableTracing: false});
+],
+{
+  enableTracing: false
+});
