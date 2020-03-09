7db17e9c7f9a6e89e1c864d6912257ff532de4be
Nassos Michas <nassosmichas@me.com>
2020-03-09 15:53:51 +0200

    Fixes #77

--- esthesis-platform-server/src/main/java/esthesis/platform/server/mapper/ApplicationMapper.java
+++ esthesis-platform-server/src/main/java/esthesis/platform/server/mapper/ApplicationMapper.java
@@ -4,8 +4,10 @@ import esthesis.platform.server.dto.ApplicationDTO;
 import esthesis.platform.server.model.Application;
 import org.mapstruct.Mapper;
 import org.mapstruct.NullValueCheckStrategy;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 
-@Mapper(componentModel = "spring", nullValueCheckStrategy = NullValueCheckStrategy.ALWAYS)
+@Mapper(componentModel = "spring", nullValueCheckStrategy = NullValueCheckStrategy.ALWAYS,
+  nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
 public abstract class ApplicationMapper extends BaseMapper<ApplicationDTO, Application> {
 
 }
