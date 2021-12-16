9145fcdc600e2e50031d6af5ec722818572669c6
Roumpen Gkoltouchtsian <roumpen.gkoltouchtsian@eurodyn.com>
2021-12-16 11:47:02 +0200

    Minor improvements for Swagger

--- esthesis-server/esthesis-server-impl/src/main/java/esthesis/platform/backend/server/config/OpenApiConfig.java
+++ esthesis-server/esthesis-server-impl/src/main/java/esthesis/platform/backend/server/config/OpenApiConfig.java
@@ -2,16 +2,12 @@ package esthesis.platform.backend.server.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@OpenAPIDefinition()
-@SecurityScheme(
-  name = "bearerAuth",
-  type = SecuritySchemeType.HTTP,
-  bearerFormat = "JWT",
-  scheme = "bearer"
-)
+@OpenAPIDefinition(info = @Info(title = "Esthesis Rest API", version = "v1"))
+@SecurityScheme(name = "bearerAuth", type = SecuritySchemeType.HTTP, bearerFormat = "JWT", scheme = "bearer")
 public class OpenApiConfig {
 }
