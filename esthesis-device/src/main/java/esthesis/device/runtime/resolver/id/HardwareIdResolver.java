622b08cfbdd0f58506ba42f37877513799d0a1ae
Nassos Michas <nassosmichas@me.com>
2022-01-12 09:11:19 +0200

    Initial import

--- esthesis-device/src/main/java/esthesis/device/runtime/resolver/id/HardwareIdResolver.java
+++
@@ -1,7 +0,0 @@
-package esthesis.device.runtime.resolver.id;
-
-import java.io.IOException;
-
-public interface HardwareIdResolver {
-  String resolve(String hardwareId) throws IOException, InterruptedException;
-}
