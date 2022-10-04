9f77cf8c6e1799231e7c3dfb91b2af63f3dd7ff9
Nassos Michas <nassosmichas@me.com>
2022-10-04 08:16:41 +0300

    chore: pre dataflows restructuring

--- server/services/srv-kubernetes/srv-kubernetes-impl/src/main/java/esthesis/service/kubernetes/impl/service/KubernetesService.java
+++ server/services/srv-kubernetes/srv-kubernetes-impl/src/main/java/esthesis/service/kubernetes/impl/service/KubernetesService.java
@@ -5,6 +5,9 @@ import io.fabric8.kubernetes.api.model.EnvVar;
 import io.fabric8.kubernetes.api.model.EnvVarBuilder;
 import io.fabric8.kubernetes.api.model.apps.Deployment;
 import io.fabric8.kubernetes.api.model.apps.DeploymentBuilder;
+import io.fabric8.kubernetes.api.model.autoscaling.v2beta2.HorizontalPodAutoscaler;
+import io.fabric8.kubernetes.api.model.autoscaling.v2beta2.HorizontalPodAutoscalerBuilder;
+import io.fabric8.kubernetes.api.model.autoscaling.v2beta2.MetricSpecBuilder;
 import io.fabric8.kubernetes.client.KubernetesClient;
 import java.util.ArrayList;
 import java.util.List;
@@ -32,32 +35,77 @@ public class KubernetesService {
   public boolean startPod(PodInfo podInfo) {
     log.debug("Starting pod '{}'.", podInfo);
 
+    //@formatter:off
     Deployment deploymentInfo = new DeploymentBuilder()
         .withNewMetadata()
-        .withName(podInfo.getName())
+          .withName(podInfo.getName())
         .endMetadata()
         .withNewSpec()
-        .withNewSelector()
-        .addToMatchLabels("app", podInfo.getName())
-        .endSelector()
-        .withNewTemplate()
-        .withNewMetadata()
-        .addToLabels("app", podInfo.getName())
-        .endMetadata()
-        .withNewSpec()
-        .addNewContainer()
-        .withName(podInfo.getName())
-        .withImage(podInfo.getImage())
-        .withImagePullPolicy("Always")
-        .withEnv(getEnvVar(podInfo))
-        .endContainer()
-        .endSpec()
-        .endTemplate()
+          .withNewSelector()
+            .addToMatchLabels("app", podInfo.getName())
+          .endSelector()
+          .withNewTemplate()
+            .withNewMetadata()
+              .addToLabels("app", podInfo.getName())
+            .endMetadata()
+            .withNewSpec()
+              .addNewContainer()
+                .withName(podInfo.getName())
+                .withImage(podInfo.getImage())
+                .withImagePullPolicy("Always")
+                .withEnv(getEnvVar(podInfo))
+              .endContainer()
+            .endSpec()
+          .endTemplate()
         .endSpec()
         .build();
+    // @formatter:on
 
-    Deployment deployment = kc.apps().deployments()
-        .inNamespace(podInfo.getNamespace()).create(deploymentInfo);
+    kc.apps().deployments().inNamespace(podInfo.getNamespace())
+        .create(deploymentInfo);
+
+    //@formatter:off
+    HorizontalPodAutoscaler horizontalPodAutoscaler = new HorizontalPodAutoscalerBuilder()
+        .withNewMetadata().withName("hpa-" + podInfo.getName())
+        .endMetadata()
+        .withNewSpec()
+        .withNewScaleTargetRef()
+        .withApiVersion("apps/v1")
+        .withKind("Deployment")
+        .withName(podInfo.getName())
+        .endScaleTargetRef()
+        .withMinReplicas(podInfo.getMinInstances())
+        .withMaxReplicas(podInfo.getMaxInstances())
+        .addToMetrics(new MetricSpecBuilder()
+            .withType("Resource")
+            .withNewResource()
+            .withName("cpu")
+            .withNewTarget()
+            .withType("Utilization")
+            .withAverageUtilization(80)
+            .endTarget()
+            .endResource()
+            .build())
+        .withNewBehavior()
+        .withNewScaleDown()
+        .addNewPolicy()
+        .withType("Pods")
+        .withValue(4)
+        .withPeriodSeconds(60)
+        .endPolicy()
+        .addNewPolicy()
+        .withType("Percent")
+        .withValue(10)
+        .withPeriodSeconds(60)
+        .endPolicy()
+        .endScaleDown()
+        .endBehavior()
+        .endSpec()
+        .build();
+    //@formatter:on
+
+    kc.autoscaling().v2beta2().horizontalPodAutoscalers().inNamespace(
+        podInfo.getNamespace()).create(horizontalPodAutoscaler);
 
     return true;
   }
---
+++ ui/src/app/dto/dataflow/dataflows.ts
@@ -0,0 +1,3 @@
+export const DATAFLOWS = {
+  TYPES: ["MQTT_CLIENT"]
+};
