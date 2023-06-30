d42b8c297ed0a4361dc12d4e663861f7102336e6
ed-devops-d2 <devops-d2@eurodyn.com>
2023-06-30 12:29:31 +0300

    docs: Fix broken links

--- esthesis-core/esthesis-core-docs/docusaurus.config.js
+++ esthesis-core/esthesis-core-docs/docusaurus.config.js
@@ -69,7 +69,6 @@ const config = {
 						{
 							type: 'docsVersionDropdown',
 							position: 'right',
-							dropdownItemsAfter: [{to: '/versions', label: 'All versions'}],
 							dropdownActiveClassDisabled: true,
 						},
 						{
