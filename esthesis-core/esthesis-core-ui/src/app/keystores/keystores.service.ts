354af4523cd4856728e8cc56510c34dcd4d923e3
Nassos Michas <nassosmichas@me.com>
2023-07-04 07:39:36 +0300

    chore: removed extra leading slash

--- esthesis-core/esthesis-core-ui/src/app/keystores/keystores.service.ts
+++ esthesis-core/esthesis-core-ui/src/app/keystores/keystores.service.ts
@@ -13,7 +13,7 @@ export class KeystoresService extends CrudDownloadService<KeystoreDto> {
   private prefix = environment.apiPrefix + "/crypto/keystore/v1";
 
   constructor(http: HttpClient, fs: FileSaverService) {
-    super(http, "/crypto/keystore/v1", fs);
+    super(http, "crypto/keystore/v1", fs);
   }
 
   download(keystoreId: string) {
