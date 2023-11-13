81e148f2bbbd0129f86fcf9f723a5d0b38e5b874
Nassos Michas <nassosmichas@me.com>
2023-11-13 13:48:56 +0200

    feat: enhance default banner for services

--- esthesis-core/esthesis-core-backend/common/src/main/java/esthesis/common/banner/BannerUtil.java
+++ esthesis-core/esthesis-core-backend/common/src/main/java/esthesis/common/banner/BannerUtil.java
@@ -23,20 +23,28 @@ https://esthes.is              esthesis@eurodyn.com"
 
 	@SuppressWarnings("java:S106")
 	public static void showBanner(String... title) {
+		// Display the common banner.
 		System.out.print(BANNER);
 
+		// Display the title if any.
 		if (title.length > 0) {
 			System.out.println("\n" + title[0]);
 		}
 
+		// Prepare version information.
 		GitUtil gitUtil = new GitUtil();
+		String buildTime =	gitUtil.getGitProperty(GitUtil.GIT_PROPERTY_BUILD_TIME).replaceAll("(.)(?=..$)", "$1:");
+		String localTime = java.time.OffsetDateTime.now().format(
+				java.time.format.DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ssXXX"));
+
+		// Display version information.
 		if (gitUtil.isInitialised()) {
 			System.out.println("Version   : " + gitUtil.getGitProperty(GitUtil.GIT_PROPERTY_VERSION));
 			System.out.println(
 				"Commit    : " + gitUtil.getGitProperty(GitUtil.GIT_PROPERTY_COMMIT_ID_FULL));
-			System.out.println("Build time: " + gitUtil.getGitProperty(GitUtil.GIT_PROPERTY_BUILD_TIME));
+			System.out.println("Build time: " + buildTime);
+			System.out.println("Local time: " + localTime);
 		}
-		System.out.println("Time zone : " + ZoneId.systemDefault());
 
 		System.out.println("***************************************************\n");
 	}
