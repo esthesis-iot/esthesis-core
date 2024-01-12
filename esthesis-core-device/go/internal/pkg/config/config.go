5f250c0bd876b386427d1d39e6822a449f0ae9b5
github-token <devops-d2@eurodyn.com>
2024-01-12 10:51:03 +0000

    release: prepare release 3.0.4

--- esthesis-core-device/go/internal/pkg/config/config.go
+++ esthesis-core-device/go/internal/pkg/config/config.go
@@ -11,7 +11,7 @@ import (
 )
 
 // Version The application version
-const Version = "3.0.4-SNAPSHOT"
+const Version = "3.0.4"
 
 // Properties received when this device was registered with the esthesis platform.
 const (
