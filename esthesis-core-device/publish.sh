84ac1e7acf8d2b81f24a465f8cc2ccbaa11d90c9
Evgenia Patsoni <evgenia.patsoni@eurodyn.com>
2024-02-13 17:40:15 +0200

    fix: automatic detection of buildkitd.toml file and untrack buildkitd.toml

--- esthesis-core-device/publish.sh
+++ esthesis-core-device/publish.sh
@@ -129,11 +129,16 @@ if [ "$ESTHESIS_BUILD_CONTAINERS" = "true" ]; then
 	# Create a Docker buildx.
   BUILDX_NAME=$(LC_CTYPE=C tr -dc 'a-zA-Z' < /dev/urandom | head -c 1)$(LC_CTYPE=C tr -dc 'a-zA-Z0-9' < /dev/urandom | head -c 11)
   printInfo "Creating Docker buildx $BUILDX_NAME."
-  if [ "$ESTHESIS_BUILDX_KUBERNETES" = "true" ]; then
-  	docker buildx create --driver kubernetes --name "$BUILDX_NAME" --use --config buildkitd.toml
+  if test -f "buildkitd.toml"; then
+      BUILDKIT_CONFIG="--config buildkitd.toml"
   else
-  	docker buildx create --name "$BUILDX_NAME" --use --config buildkitd.toml
-	fi
+      BUILDKIT_CONFIG=""
+  fi
+  if [ "$ESTHESIS_BUILDX_KUBERNETES" = "true" ]; then
+    docker buildx create --driver kubernetes --name "$BUILDX_NAME" --use $BUILDKIT_CONFIG
+  else
+    docker buildx create --name "$BUILDX_NAME" --use $BUILDKIT_CONFIG
+  fi
 
   # Login to remote registry.
   if [ -n "$ESTHESIS_REGISTRY_USERNAME" ] && [ -n "$ESTHESIS_REGISTRY_PASSWORD" ]; then
