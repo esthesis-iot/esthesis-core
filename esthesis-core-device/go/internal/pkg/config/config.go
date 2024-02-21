69c3c53ad965b9c6eb506eab89243b288bc8a7e7
Nassos Michas <nassosmichas@me.com>
2024-02-21 16:08:59 +0200

    release: prepare release 3.0.12

--- esthesis-core-device/go/internal/pkg/config/config.go
+++ esthesis-core-device/go/internal/pkg/config/config.go
@@ -11,7 +11,7 @@ import (
 )
 
 // Version The application version
-const Version = "3.0.10-SNAPSHOT"
+const Version = "3.0.12"
 
 // Properties received when this device was registered with the esthesis platform.
 const (
