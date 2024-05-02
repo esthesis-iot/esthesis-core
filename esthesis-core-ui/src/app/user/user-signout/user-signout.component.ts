8b8ebe8b2ca35adce44416086dfc79835a0c677d
Nassos Michas <nassosmichas@me.com>
2024-05-02 06:23:06 +0300

    fix: switch signout to erase tokens

--- esthesis-core-ui/src/app/user/user-signout/user-signout.component.ts
+++ esthesis-core-ui/src/app/user/user-signout/user-signout.component.ts
@@ -1,15 +1,24 @@
 import {Component} from "@angular/core";
 import {OidcSecurityService} from "angular-auth-oidc-client";
 import {Router} from "@angular/router";
+import {UtilityService} from "../../shared/services/utility.service";
 
 @Component({
-  selector: 'app-user-signout',
-  templateUrl: './user-signout.component.html'
+  selector: "app-user-signout",
+  templateUrl: "./user-signout.component.html"
 })
 export class UserSignoutComponent {
 
-  constructor(private oidcSecurityService: OidcSecurityService, private router: Router) {
-    oidcSecurityService.logoffLocal();
-    this.router.navigate(["dashboard"]);
+  constructor(private oidcSecurityService: OidcSecurityService, private router: Router,
+    private utilityService: UtilityService) {
+    this.oidcSecurityService.logoffAndRevokeTokens().subscribe({
+      next: () => {
+        this.router.navigate(["dashboard"]);
+      },
+      error: (err) => {
+        this.utilityService.popupError("An error occurred while signing out: " + err);
+        this.router.navigate(["dashboard"]);
+      }
+    });
   }
 }
