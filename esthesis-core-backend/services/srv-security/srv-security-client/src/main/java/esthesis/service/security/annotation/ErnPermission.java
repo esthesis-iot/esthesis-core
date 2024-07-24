ab150115d81bb3be5a713bcb7363a9eb8cb5baf7
Nassos Michas <nassosmichas@me.com>
2024-07-24 07:23:58 +0300

    fix: maven versions

--- esthesis-core-backend/services/srv-security/srv-security-client/src/main/java/esthesis/service/security/annotation/ErnPermission.java
+++ esthesis-core-backend/services/srv-security/srv-security-client/src/main/java/esthesis/service/security/annotation/ErnPermission.java
@@ -2,7 +2,6 @@ package esthesis.service.security.annotation;
 
 import esthesis.common.AppConstants.Security.Category;
 import esthesis.common.AppConstants.Security.Operation;
-import jakarta.enterprise.util.Nonbinding;
 import jakarta.interceptor.InterceptorBinding;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
@@ -22,18 +21,18 @@ public @interface ErnPermission {
 	// A list of roles for which security evaluation should be bypassed. This is useful for some
 	// rare cases, where a method is used both by an authenticated user and an unauthenticated user
 	// (mainly, system calls).
-	@Nonbinding
 	String[] bypassForRoles() default {};
 
 	// The category of the security operation.
-	@Nonbinding
 	Category category();
 
 	// The operation to be performed.
-	@Nonbinding
 	Operation operation();
 
-	// The resource ID, if any.
-	@Nonbinding
-	String resourceId() default "";
+	// Specifies whether the security check should include a check for the resource id being
+	// accessed. If this is set to 'true', it is expected that the resource id can be found as the
+	// first parameter of the method being annotated. The resource id can be either a string or a
+	// BaseEntity object. If the resource id can not be successfully extracted, a warning will be
+	// logged and the security check will be performed without including the resource id.
+	boolean checkResourceId() default true;
 }
