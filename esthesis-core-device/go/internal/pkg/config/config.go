f81884c4788390626da01079f9c666172e7d88d5
ED DevSecOps <devops-d2@eurodyn.com>
2024-08-22 16:19:45 +0000

    release: prepare release 3.0.50

--- esthesis-core-device/go/internal/pkg/config/config.go
+++ esthesis-core-device/go/internal/pkg/config/config.go
@@ -11,7 +11,7 @@ import (
 )
 
 // Version The application version
-const Version = "3.0.50-SNAPSHOT"
+const Version = "3.0.50"
 
 // Properties received when this device was registered with the esthesis platform.
 const (
