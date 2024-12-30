aedfc20caa53cab6b01ea6237f560fcb33a6f5bd
Pablo Mezzon Kintopp <pablokintopp@gmail.com>
2024-12-30 10:59:51 -0300

    chore: dataflow service tests

---
+++ esthesis-core-backend/services/srv-dataflow/srv-dataflow-impl/src/test/java/esthesis/service/dataflow/impl/service/DataflowServiceTest.java
@@ -0,0 +1,152 @@
+package esthesis.service.dataflow.impl.service;
+
+import esthesis.common.exception.QDoesNotExistException;
+import esthesis.service.dataflow.dto.FormlySelectOption;
+import esthesis.service.dataflow.entity.DataflowEntity;
+import esthesis.service.kubernetes.dto.DeploymentInfoDTO;
+import esthesis.service.kubernetes.resource.KubernetesResource;
+import io.quarkus.test.InjectMock;
+import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.test.junit.mockito.MockitoConfig;
+import jakarta.inject.Inject;
+import lombok.extern.slf4j.Slf4j;
+import org.bson.types.ObjectId;
+import org.eclipse.microprofile.rest.client.inject.RestClient;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
+@Slf4j
+@QuarkusTest
+class DataflowServiceTest {
+
+	@Inject
+	TestHelper testHelper;
+
+	@Inject
+	DataflowService dataflowService;
+
+	@InjectMock
+	@MockitoConfig(convertScopes = true)
+	@RestClient
+	KubernetesResource kubernetesResource;
+
+	int initialDataflowSizeInDB = 0;
+
+	@BeforeEach
+	void setUp() {
+		testHelper.clearDatabase();
+		testHelper.createDataflow();
+		initialDataflowSizeInDB = testHelper.findAllDataflowEntity().size();
+
+		log.info("Initial dataflow size in DB: {}", initialDataflowSizeInDB);
+
+		// Mock the kubernetes resource requests
+		when(kubernetesResource.scheduleDeployment(Mockito.any(DeploymentInfoDTO.class))).thenReturn(true);
+		when(kubernetesResource.getNamespaces()).thenReturn(testHelper.getNamespaces());
+		when(kubernetesResource.isDeploymentNameAvailable(anyString(), anyString())).thenReturn(true);
+	}
+
+	@Test
+	void getNamespaces() {
+		// Arrange
+		int expectedSize = testHelper.getNamespaces().size();
+
+		// Act
+		List<FormlySelectOption> namespaces = dataflowService.getNamespaces();
+
+		// Assert
+		assertEquals(expectedSize, namespaces.size());
+	}
+
+	@Test
+	void delete() {
+		// Arrange
+		String nonexistentDataflowId = new ObjectId().toString();
+		String validDataflowId = testHelper.findOneDataflowEntity().getId().toString();
+
+		// Act try to delete nonexistent dataflow
+		assertThrows(QDoesNotExistException.class, () -> dataflowService.delete(nonexistentDataflowId));
+
+		// Assert dataflow is not deleted
+		assertEquals(initialDataflowSizeInDB, testHelper.findAllDataflowEntity().size());
+
+		// Act try to delete valid dataflow
+		dataflowService.delete(validDataflowId);
+
+		// Assert dataflow is deleted
+		assertEquals(initialDataflowSizeInDB - 1, testHelper.findAllDataflowEntity().size());
+	}
+
+	@Test
+	void saveNew() {
+		// Arrange
+		DataflowEntity newDataflow = new DataflowEntity();
+		newDataflow.setName("new dataflow");
+		newDataflow.setType("new type");
+		newDataflow.setStatus(true);
+		newDataflow.setConfig(testHelper.createDataflowConfig());
+
+		// Act
+		DataflowEntity savedDataflow = dataflowService.saveNew(newDataflow);
+
+		// Assert dataflow is saved
+		assertEquals(initialDataflowSizeInDB + 1, testHelper.findAllDataflowEntity().size());
+	}
+
+
+	@Test
+	void saveUpdate() {
+		// Arrange
+		DataflowEntity existingDataflow = testHelper.findOneDataflowEntity();
+		existingDataflow.setName("updated dataflow");
+		existingDataflow.setType("updated type");
+		existingDataflow.setStatus(false);
+
+		// Act
+		DataflowEntity savedDataflow = dataflowService.saveUpdate(existingDataflow);
+
+		// Assert dataflow is updated
+		assertEquals(initialDataflowSizeInDB, testHelper.findAllDataflowEntity().size());
+		assertEquals(existingDataflow.getId(), savedDataflow.getId());
+	}
+
+	@Test
+	void find() {
+		// Act
+		List<DataflowEntity> dataflowEntities =
+			dataflowService.find(testHelper.makePageable(0, 100), true).getContent();
+
+		// Assert
+		assertEquals(initialDataflowSizeInDB, dataflowEntities.size());
+
+	}
+
+	@Test
+	void findById() {
+		//Arrange
+		String validDataflowId = testHelper.findOneDataflowEntity().getId().toString();
+		String nonexistentDataflowId = new ObjectId().toString();
+
+		// Act & Assert
+		assertNull(dataflowService.findById(nonexistentDataflowId));
+		assertNotNull(dataflowService.findById(validDataflowId));
+
+	}
+
+	@Test
+	void isDeploymentNameAvailable() {
+		// Act & Assert
+		assertTrue(dataflowService.isDeploymentNameAvailable("test-name", "test-namespace"));
+	}
+}
---
+++ esthesis-core-backend/services/srv-dataflow/srv-dataflow-impl/src/test/java/esthesis/service/dataflow/impl/service/TestHelper.java
@@ -0,0 +1,102 @@
+package esthesis.service.dataflow.impl.service;
+
+import esthesis.service.common.paging.Pageable;
+import esthesis.service.dataflow.entity.DataflowEntity;
+import esthesis.service.dataflow.impl.repository.DataflowRepository;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.core.MultivaluedHashMap;
+import jakarta.ws.rs.core.UriInfo;
+import org.mockito.Mockito;
+
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.mockito.Mockito.when;
+
+@ApplicationScoped
+public class TestHelper {
+
+	@Inject
+	DataflowRepository dataflowRepository;
+
+	public DataflowEntity createDataflow() {
+		DataflowEntity dataflow = new DataflowEntity();
+		dataflow.setName("test dataflow");
+		dataflow.setType("test type");
+		dataflow.setConfig(createDataflowConfig());
+		dataflow.setStatus(true);
+		dataflowRepository.persist(dataflow);
+		return dataflow;
+	}
+
+	// Create a configuration map that matches the expected structure for the dataflow configuration
+	public Map<String, Object> createDataflowConfig() {
+		// Kubernetes-specific configuration
+		Map<String, Object> kubernetesConfig = new HashMap<>();
+
+		// Secrets configuration
+		List<Map<String, String>> secrets = new ArrayList<>();
+		secrets.add(Map.of(
+			"name", "exampleSecret", // Aligning with SECRET_NAME
+			"path", "/path/to/secret", // Aligning with SECRET_PATH
+			"content", "exampleContent" // Aligning with SECRET_CONTENT
+		));
+		kubernetesConfig.put("secrets", secrets);
+
+		// Other Kubernetes configuration details
+		kubernetesConfig.put("namespace", "test-namespace");
+		kubernetesConfig.put("container-image-version", "1.0.0");
+		kubernetesConfig.put("pods-min", 1);
+		kubernetesConfig.put("pods-max", 5);
+		kubernetesConfig.put("cpu-request", "500m");
+		kubernetesConfig.put("cpu-limit", "1");
+		kubernetesConfig.put("registry", "docker.registry.url");
+		kubernetesConfig.put("env", "ENV_VAR1=value1\nENV_VAR2=value2");
+
+		// Flattened map inclusion
+		Map<String, Object> config = new HashMap<>();
+		config.put("kubernetes", kubernetesConfig);
+		return config;
+	}
+
+
+	public void clearDatabase() {
+		dataflowRepository.deleteAll();
+	}
+
+	public List<DataflowEntity> findAllDataflowEntity() {
+		return dataflowRepository.listAll();
+	}
+
+	public List<String> getNamespaces() {
+		return List.of("test-namespace-1", "test-namespace-2", "test-namespace-3");
+	}
+
+	public DataflowEntity findOneDataflowEntity() {
+		return dataflowRepository.findAll().firstResult();
+	}
+
+	/**
+	 * Helper method to create a Pageable object with the specified parameters
+	 */
+	public Pageable makePageable(int page, int size) {
+
+		// Create a mock of UriInfo
+		UriInfo uriInfo = Mockito.mock(UriInfo.class);
+
+		// Define the behavior of the mock
+		when(uriInfo.getRequestUri()).thenReturn(URI.create("http://localhost:8080/find?page=" + page + "&size=" + size));
+		when(uriInfo.getQueryParameters()).thenReturn(new MultivaluedHashMap<>());
+
+		Pageable pageable = new Pageable();
+		pageable.setPage(page);
+		pageable.setSize(size);
+		pageable.setSort("");
+		pageable.setUriInfo(uriInfo);
+		return pageable;
+	}
+}
