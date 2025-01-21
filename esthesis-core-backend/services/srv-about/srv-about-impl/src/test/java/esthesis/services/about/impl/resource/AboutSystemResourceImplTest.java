58f31cf7838b4b132476d36b4797a72c015911e8
Nassos Michas <nassosmichas@me.com>
2025-01-21 15:00:49 +0200

    chore: tests and versions bump

---
+++ esthesis-core-backend/services/srv-about/srv-about-impl/src/test/java/esthesis/services/about/impl/resource/AboutSystemResourceImplTest.java
@@ -0,0 +1,47 @@
+package esthesis.services.about.impl.resource;
+
+import static io.restassured.RestAssured.when;
+import static org.hamcrest.CoreMatchers.notNullValue;
+
+import esthesis.core.common.AppConstants;
+import esthesis.service.about.dto.AboutGeneralDTO;
+import esthesis.service.about.resource.AboutResource;
+import io.quarkus.test.common.http.TestHTTPEndpoint;
+import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.test.security.TestSecurity;
+import org.junit.jupiter.api.Test;
+
+@QuarkusTest
+@TestHTTPEndpoint(AboutResource.class)
+class AboutSystemResourceImplTest {
+
+	@Test
+	@TestSecurity(user = "test-user", roles = AppConstants.ROLE_SYSTEM)
+	void testAboutOK() {
+		when().get("/v1/system/general")
+			.then()
+			.contentType("application/json")
+			.body(notNullValue(AboutGeneralDTO.class))
+			.body("gitBuildTime", notNullValue())
+			.body("gitCommitId", notNullValue())
+			.body("gitCommitIdAbbrev", notNullValue())
+			.body("gitVersion", notNullValue())
+			.statusCode(200);
+	}
+
+	@Test
+	void testAboutNOK() {
+		when().get("/v1/system/general")
+			.then()
+			.statusCode(401);
+	}
+
+	@Test
+	@TestSecurity(user = "test-user", roles = AppConstants.ROLE_USER)
+	void testAboutNOK2() {
+		when().get("/v1/system/general")
+			.then()
+			.statusCode(403);
+	}
+
+}
