035a3e639c9279e6e70520167f33ffb9ff5e7526
Pablo Mezzon Kintopp <pablokintopp@gmail.com>
2025-02-05 12:46:36 -0300

    chore: tag services tests

---
+++ esthesis-core-backend/services/srv-tag/srv-tag-impl/src/test/java/esthesis/services/tag/impl/TestHelper.java
@@ -0,0 +1,48 @@
+package esthesis.services.tag.impl;
+
+import esthesis.service.common.paging.Pageable;
+import esthesis.service.tag.entity.TagEntity;
+import esthesis.services.tag.impl.repository.TagRepository;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.core.MultivaluedHashMap;
+import jakarta.ws.rs.core.UriInfo;
+import org.mockito.Mockito;
+
+import java.net.URI;
+
+import static org.mockito.Mockito.when;
+
+@ApplicationScoped
+public class TestHelper {
+
+	@Inject
+	TagRepository tagRepository;
+
+	public void clearDatabase() {
+		tagRepository.deleteAll();
+	}
+
+	/**
+	 * Mock a Pageable object with the specified parameters.
+	 *
+	 * @param page The page number being requested.
+	 * @param size The size of the page.
+	 * @return The mocked Pageable object.
+	 */
+	public Pageable makePageable(int page, int size) {
+
+		// Mock the request URI and parameters.
+		UriInfo uriInfo = Mockito.mock(UriInfo.class);
+		when(uriInfo.getRequestUri()).thenReturn(URI.create("http://localhost:8080/find?page=" + page + "&size=" + size));
+		when(uriInfo.getQueryParameters()).thenReturn(new MultivaluedHashMap<>());
+
+		Pageable pageable = new Pageable();
+		pageable.setPage(page);
+		pageable.setSize(size);
+		pageable.setSort("");
+		pageable.setUriInfo(uriInfo);
+		return pageable;
+	}
+
+}
